afaf6ef58b996ce0a2673e0717853663e39a9764
HenriqueLDSa <rique.ldsa@gmail.com>
2024-11-24 01:04:47 -0500

    added AlertDialog as a form to add a trip, and implemented API request to POST the trip

--- lib/home_page_content.dart
+++ lib/home_page_content.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'trip.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePageContent extends StatefulWidget {
   const HomePageContent({super.key});
@@ -13,7 +16,31 @@ class HomePageContent extends StatefulWidget {
 }
 
 class _HomePageContentState extends State<HomePageContent> {
+  var logger = Logger(
+    printer: PrettyPrinter(),
+  );
+
+  var loggerNoStack = Logger(
+    printer: PrettyPrinter(methodCount: 0),
+  );
+
+  final TextEditingController _tripNameController = TextEditingController();
+  final TextEditingController _tripLocationController = TextEditingController();
+  final TextEditingController _tripStartDateController =
+      TextEditingController();
+  final TextEditingController _tripEndDateController = TextEditingController();
+  final TextEditingController _tripNotesController = TextEditingController();
+  final GlobalKey _choosePhotoButtonKey = GlobalKey();
+  final TextEditingController _photoNameController =
+      TextEditingController(text: 'No photo selected');
+
   Future<List<Trip>>? _futureItems;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTrips();
+  }
 
   Future<List<Trip>> fetchTrips(String userId) async {
     final response =
@@ -23,23 +50,249 @@ class _HomePageContentState extends State<HomePageContent> {
       List jsonResponse = json.decode(response.body);
       return jsonResponse.map((item) => Trip.fromJson(item)).toList();
     } else {
+      logger.e('Failed to fetch trips: ${response.body}');
       throw Exception('Failed to load trips');
     }
   }
 
   Future<void> _loadTrips() async {
     final prefs = await SharedPreferences.getInstance();
-    final String? userId = prefs.getString('userId'); // Not being used for now
+    final String? userId = prefs.getString('userId');
 
     setState(() {
-      _futureItems = fetchTrips(userId!); // Use hardcoded userId
+      _futureItems = fetchTrips(userId!);
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _loadTrips();
+  Future<void> _addTrip(String reqName, String reqCity, String reqStartDate,
+      String reqEndDate, String reqNotes, String reqPicName) async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? userId = prefs.getString('userId');
+
+    final Map<String, dynamic> payload = {
+      'name': reqName,
+      'city': reqCity,
+      'start_date': reqStartDate,
+      'end_date': reqEndDate,
+      'notes': reqNotes,
+      'picture_url': reqPicName
+    };
+
+    final response = await http.post(
+      Uri.parse('https://xplora.fun/api/users/$userId/trips'),
+      body: jsonEncode(payload),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    logger.d('Payload addTrip: $payload');
+
+    final jsonResponse = json.decode(response.body);
+
+    if (response.statusCode == 201) {
+      Fluttertoast.showToast(
+        msg: jsonResponse['message'],
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+      );
+
+      _loadTrips();
+    } else {
+      Fluttertoast.showToast(
+        msg: jsonResponse['error'],
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+      );
+    }
+  }
+
+  void _pickImage(ImageSource source, void Function(void Function()) setState) {
+    final ImagePicker picker = ImagePicker();
+    picker.pickImage(source: source).then((pickedFile) {
+      if (pickedFile != null) {
+        setState(() {
+          _photoNameController.text = pickedFile.name;
+        });
+      }
+    });
+  }
+
+  void _showAddDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Add Trip Details'),
+          content: StatefulBuilder(
+            builder: (context, setState) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: _tripNameController,
+                    decoration: InputDecoration(
+                      labelText: 'Trip Name',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    controller: _tripLocationController,
+                    decoration: InputDecoration(
+                      labelText: 'Location',
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    controller: _tripStartDateController,
+                    decoration: InputDecoration(
+                      labelText: 'Start Date',
+                      border: OutlineInputBorder(),
+                    ),
+                    readOnly: true,
+                    onTap: () => {
+                      showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2100),
+                      ).then((pickedDate) {
+                        if (pickedDate != null) {
+                          _tripStartDateController.text =
+                              "${pickedDate.toLocal()}".split(' ')[0];
+                        }
+                      })
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    controller: _tripEndDateController,
+                    decoration: InputDecoration(
+                      labelText: 'End Date',
+                      border: OutlineInputBorder(),
+                    ),
+                    readOnly: true,
+                    onTap: () => {
+                      showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2100),
+                      ).then((pickedDate) {
+                        if (pickedDate != null) {
+                          _tripEndDateController.text =
+                              "${pickedDate.toLocal()}".split(' ')[0];
+                        }
+                      })
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    controller: _tripNotesController,
+                    decoration: InputDecoration(
+                      labelText: 'Notes',
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    key: _choosePhotoButtonKey,
+                    onPressed: () async {
+                      final RenderBox renderBox = _choosePhotoButtonKey
+                          .currentContext!
+                          .findRenderObject() as RenderBox;
+                      final position = renderBox.localToGlobal(
+                          Offset.zero); // Get global position of the button
+
+                      // Show the dropdown menu when the button is clicked
+                      showMenu<String>(
+                        context: context,
+                        position: RelativeRect.fromLTRB(
+                            position.dx,
+                            position.dy + renderBox.size.height,
+                            position.dx + renderBox.size.width,
+                            0.0),
+                        items: [
+                          PopupMenuItem<String>(
+                            value: 'Take picture',
+                            child: Text('Take picture'),
+                          ),
+                          PopupMenuItem<String>(
+                            value: 'Select from gallery',
+                            child: Text('Select from gallery'),
+                          ),
+                        ],
+                      ).then((value) {
+                        if (value != null) {
+                          if (value == 'Take picture') {
+                            _pickImage(ImageSource.camera, setState);
+                          } else if (value == 'Select from gallery') {
+                            _pickImage(ImageSource.gallery, setState);
+                          }
+                        }
+                      });
+                    },
+                    child: Text('Choose trip photo'),
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(
+                        maxWidth: 200), // Set max width for the text
+                    child: Text(
+                      _photoNameController.text.isEmpty
+                          ? 'No photo selected'
+                          : _photoNameController.text,
+                      maxLines: 1,
+                      overflow:
+                          TextOverflow.ellipsis, // Truncate text with ellipsis
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  )
+                ],
+              );
+            },
+          ),
+          actionsAlignment: MainAxisAlignment.spaceBetween,
+          actions: [
+            TextButton(
+              onPressed: () {
+                _tripNameController.clear();
+                _tripLocationController.clear();
+                _tripStartDateController.clear();
+                _tripEndDateController.clear();
+                _tripNotesController.clear();
+                _photoNameController.clear();
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _addTrip(
+                    _tripNameController.text,
+                    _tripLocationController.text,
+                    _tripStartDateController.text,
+                    _tripEndDateController.text,
+                    _tripNotesController.text,
+                    _photoNameController.text);
+                Navigator.of(context).pop();
+                _tripNameController.clear();
+                _tripLocationController.clear();
+                _tripStartDateController.clear();
+                _tripEndDateController.clear();
+                _tripNotesController.clear();
+                _photoNameController.clear();
+              },
+              child: Text('Submit'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -65,10 +318,15 @@ class _HomePageContentState extends State<HomePageContent> {
             Padding(
               padding: EdgeInsets.only(right: 16.0),
               child: Center(
-                child: Icon(
-                  Icons.add,
-                  color: Color(0xFF6A0DAD),
-                  size: 43.0,
+                child: IconButton(
+                  onPressed: () {
+                    _showAddDialog();
+                  },
+                  icon: Icon(
+                    Icons.add,
+                    color: Color(0xFF6A0DAD),
+                    size: 43.0,
+                  ),
                 ),
               ),
             ),
